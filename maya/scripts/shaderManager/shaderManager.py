84a3522d48d98fb475cab51b1f3a83d6f41bf9b9
Elliott <esmith@milk-vfx.com>
2018-02-19 16:42:59 +0000

    Revert "Fix shader manager"
    This reverts commit cd43ab39538064ce12f3736efa2bb77e30bd8dfc.

--- maya/scripts/shaderManager/shaderManager.py
+++ maya/scripts/shaderManager/shaderManager.py
@@ -402,7 +402,7 @@ class ShaderManager(QtWidgets.QMainWindow, UI_ABCHierarchy.Ui_NAM):
                     for cache in self.ABCViewerNode.values():
                         cache.renameShader(prevName, nodeName)
                     self.checkShaders()
-                    self.shaders.update()
+                    self.updateShaders()
 
             # shader manager isnt open, update the attributes directly
             else:
@@ -425,7 +425,7 @@ class ShaderManager(QtWidgets.QMainWindow, UI_ABCHierarchy.Ui_NAM):
 
     def newNodeCB(self, newNode, data ):
         ''' Callback when creating a new node '''
-        self.shaders.update()
+        self.updateShaders()
 
     def delNodeCB(self, node, data ):
         ''' Callback when a node has been deleted '''
@@ -442,7 +442,7 @@ class ShaderManager(QtWidgets.QMainWindow, UI_ABCHierarchy.Ui_NAM):
                 cache.removeShader(nodeName)                
             
             if didSomething:
-                self.shaders.update()
+                self.updateShaders()
                 self.checkShaders()
 
     def newshadersListmouseMoveEvent(self, event):

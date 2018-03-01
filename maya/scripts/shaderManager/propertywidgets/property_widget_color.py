d42c0616af5f789140399ee94a4ddcddcd7e7ef6
Elliott <esmith@milk-vfx.com>
2018-03-01 11:06:01 +0000

    Replacing PySide color picker with maya color managed color picker.

--- maya/scripts/shaderManager/propertywidgets/property_widget_color.py
+++ maya/scripts/shaderManager/propertywidgets/property_widget_color.py
@@ -16,24 +16,25 @@ from PySide2.QtCore import *
 from PySide2.QtWidgets import *
 from arnold import *
 from property_widget import *
+import maya.cmds as cmds
+
 class PropertyWidgetColor(PropertyWidget):
    def __init__(self, controller,  params, colorType, parent = None):
       PropertyWidget.__init__(self, params, parent)
 
       self.paramName = params["name"]
       self.colorType = colorType
-
       self.controller = controller
 
       self.widget = QPushButton(self)
       self.widget.setFlat(True)
       self.widget.setAutoFillBackground(True)
-      self.widget.clicked.connect(self.ShowColorDialog)
+
+      self.widget.clicked.connect(self.mayaColorPicker)
       self.layout().addWidget(self.widget)
 
       self.default = params["value"]
       self.default_color = QColor(self.default[0] * 255, self.default[1] * 255, self.default[2]* 255)
-      self.colorDialog = QColorDialog(self)
       self.ColorChanged(self.default_color)
 
    def ColorChanged(self, color):
@@ -41,35 +42,30 @@ class PropertyWidgetColor(PropertyWidget):
       palette.setColor(QPalette.Button, color)
       self.widget.setPalette(palette)
 
-   def ShowColorDialog(self):
-      try:
-         self.colorDialog.ValueChanged.disconnect()
-      except:
-         pass
-      self.colorDialog.currentColorChanged.connect(self.ValueChanged)
-      color = self.widget.palette().color(QPalette.Button)
-      self.colorDialog.setCurrentColor(color)
-      self.colorDialog.setOption(QColorDialog.ShowAlphaChannel, self.colorType == PropertyWidget.RGBA)
-      self.colorDialog.show()
+   def mayaColorPicker(self):
+      """"""
+      cmds.colorEditor()
+      if cmds.colorEditor(query=True, result=True):
+         value = []
+         values = cmds.colorEditor(query=True, rgb=True)
 
+         value.append( values[0])
+         value.append( values[1])
+         value.append( values[2])
 
-   def ValueChanged(self, color):
-      self.ColorChanged(color)
-      value = []
-      value.append( color.redF())
-      value.append( color.greenF())
-      value.append( color.blueF())
-      if  self.colorType == PropertyWidget.RGBA:
-         value.append( color.alphaF())
-      self.controller.mainEditor.propertyChanged(dict(propname=self.paramName, default=value == self.default, value=value))
+         if  self.colorType == PropertyWidget.RGBA:
+            value.append( color.alphaF())
+         self.controller.mainEditor.propertyChanged(dict(propname=self.paramName, default=value == self.default, value=value))
+
+         color = QColor(value[0]*255, value[1]*255, value[2]*255)
+         self.ColorChanged(color)        
 
    def changed(self, message):
       value = message["value"]
       color = QColor(value[0]*255, value[1]*255, value[2]*255)
       self.ColorChanged(color)
 
-
    def resetValue(self):
-    self.ValueChanged(self.default_color)
+      self.ColorChanged(self.default_color)
 
 
